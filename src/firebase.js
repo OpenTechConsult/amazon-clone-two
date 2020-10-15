1e39d1b33f628fef90ea73a101ff27a2044cd672
Elom Atsou Agboka <opentech.consult@gmail.com>
2020-10-15 17:26:15 +0000

    Add a file call firebase.js in which we have the required configuration to work with firebase

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAT72g6kJTahT_zjLiUzCxf21cGuKjz2TI",
+    authDomain: "clone-two-759eb.firebaseapp.com",
+    databaseURL: "https://clone-two-759eb.firebaseio.com",
+    projectId: "clone-two-759eb",
+    storageBucket: "clone-two-759eb.appspot.com",
+    messagingSenderId: "160446580416",
+    appId: "1:160446580416:web:bfbcee108d76524f4ccf25",
+    measurementId: "G-YX4EMGYTHH"
+  };
